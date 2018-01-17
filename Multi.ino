f9f0b40d0085ba49ddc38dd09632d768a17549b9
Beschdl <33841601+Beschdl@users.noreply.github.com>
2018-01-17 14:05:22 +0100

    Update Multi.ino

--- Multi.ino
+++ Multi.ino
@@ -1,16 +1,16 @@
 /*
   #################################
-  #    MESSGERÄT                  #
-  # Messbare Einheiten:           #
-  #   -Temperatur                 #    
-  # -Lichtverhältnisse            #
-  # -Luftfeutchtigkeit            #
-  # -Geschwindigkeit              #
-  # -Geschräusche bzw.            #
-  #   Lautstärke                  #
-  #   -Zeit                       #
-  #   -Länge                      #
-  #  +integrierte Uhr             #
+  #      MESSGERÄT                #
+  #   Messbare Einheiten:         #
+  #    -Temperatur                #    
+  #   -Lichtverhältnisse          #
+  #   -Luftfeutchtigkeit          #
+  #   -Geschwindigkeit            #
+  #   -Geschräusche bzw.          #
+  #    Lautstärke                 #
+  #    -Zeit                      #
+  #    -Länge                     #
+  #    +integrierte Uhr           #
   #################################
 */
 //  ######## BASIC LIBRARIES
